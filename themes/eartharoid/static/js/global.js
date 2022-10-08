5d0049e3d1781f086832440bc3e866676bfcbdf6
David Ralph <me@davidcralph.co.uk>
2022-10-08 11:15:29 +0100

    various fixes, switch to pnpm, formatting etc

--- themes/eartharoid/static/js/global.js
+++ themes/eartharoid/static/js/global.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 	// Get all 'navbar-burger' elements
 	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
 	// Check if there are any navbar burgers
 	if ($navbarBurgers.length > 0) {
-
 		// Add a click event on each of them
 		$navbarBurgers.forEach(el => {
 			el.addEventListener('click', () => {
 
 				// Get the target from the 'data-target' attribute
-				const target = el.dataset.target;
-				const $target = document.getElementById(target);
+				const $target = document.getElementById(el.dataset.target);
 
 				// Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
 				el.classList.toggle('is-active');
 				$target.classList.toggle('is-active');
-
 			});
 		});
 	}
@@ -27,15 +23,12 @@ if (!localStorage.getItem('theme')) {
 		window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 }
 
-let theme = localStorage.getItem('theme');
+const theme = localStorage.getItem('theme');
 
-switch (theme) {
-	case 'dark':
-		enableDarkTheme();
-		break;
-	case 'light':
-		enableLightTheme();
-		break;
+if (theme === 'dark') {
+	enableLightTheme();
+} else {
+	enableDarkTheme();
 }
 
 document.getElementById('darkThemeCSS').remove();
@@ -44,7 +37,7 @@ document.getElementById('lightThemeCSS').remove();
 function enableDarkTheme() {
 	localStorage.setItem('theme', 'dark');
 
-	let icon = document.getElementById('themeIcon');
+	const icon = document.getElementById('themeIcon');
 	icon.classList.add('fa-moon');
 	icon.classList.remove('fa-sun');
 
@@ -58,11 +51,11 @@ function enableDarkTheme() {
 function enableLightTheme() {
 	localStorage.setItem('theme', 'light');
 
-	let icon = document.getElementById('themeIcon');
+	const icon = document.getElementById('themeIcon');
 	icon.classList.add('fa-sun');
 	icon.classList.remove('fa-moon');
 
-	let link = document.getElementById('themeCSS');
+	const link = document.getElementById('themeCSS');
 	link.setAttribute('href', '/css/light_theme.css');
 
 	document.getElementById('madeWithBulma')
@@ -70,14 +63,13 @@ function enableLightTheme() {
 }
 
 function toggleTheme() {
-	let theme = localStorage.getItem('theme');
+	const theme = localStorage.getItem('theme');
 
-	switch (theme) {
-		case 'dark':
-			enableLightTheme(); // it is dark, make it light theme
-			break;
-		case 'light':
-			enableDarkTheme(); // it is light, make it dark theme
-			break;
+	// if it's dark, make it light theme
+	// else set it back to dark again
+	if (theme === 'dark') {
+		enableLightTheme();
+	} else {
+		enableDarkTheme();
 	}
 }
